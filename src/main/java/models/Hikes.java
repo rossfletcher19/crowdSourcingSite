f2b4361c1d9b26f0265c870edecd22a8f91b3f07
Ross Fletcher <rtbfletch@outlook.com>
2018-01-18 16:02:54 -0800

    add hikes.java props, getters and setters, hashcode and equals

--- src/main/java/models/Hikes.java
+++ src/main/java/models/Hikes.java
@@ -4,6 +4,90 @@ package models;
  * Created by Guest on 1/18/18.
  */
 public class Hikes {
+    private String nameOfHike;
+    private String locationOfHike;
+    private String notesOnHike;
+    private int ratingHike;
+    private boolean completedHike;
+    private int id;
+    private int locationId;
 
+    public Hikes(String nameOfHike, String locationOfHike, String notesOnHike, int ratingHike, int locationId) {
+        this.nameOfHike = nameOfHike;
+        this.locationOfHike = locationOfHike;
+        this.notesOnHike = notesOnHike;
+        this.ratingHike = ratingHike;
+        this.completedHike = completedHike;
+        this.locationId = locationId;
+    }
 
+    // Getters
+
+    public String getNameOfHike() {
+        return nameOfHike;
+    }
+
+    public String getLocationOfHike() {
+        return locationOfHike;
+    }
+
+    public String getNotesOnHike() {
+        return notesOnHike;
+    }
+
+    public int getRatingHike() {
+        return ratingHike;
+    }
+
+    public int getLocationId() {
+        return locationId;
+    }
+
+    // Setters
+
+    public void setNameOfHike(String nameOfHike) {
+        this.nameOfHike = nameOfHike;
+    }
+
+    public void setLocationOfHike(String locationOfHike) {
+        this.locationOfHike = locationOfHike;
+    }
+
+    public void setNotesOnHike(String notesOnHike) {
+        this.notesOnHike = notesOnHike;
+    }
+
+    public void setRatingHike(int ratingHike) {
+        this.ratingHike = ratingHike;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setLocationId(int locationId) {
+        this.locationId = locationId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Hikes hikes = (Hikes) o;
+
+        if (ratingHike != hikes.ratingHike) return false;
+        if (completedHike != hikes.completedHike) return false;
+        if (!nameOfHike.equals(hikes.nameOfHike)) return false;
+        return locationOfHike.equals(hikes.locationOfHike);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = nameOfHike.hashCode();
+        result = 31 * result + locationOfHike.hashCode();
+        result = 31 * result + ratingHike;
+        result = 31 * result + (completedHike ? 1 : 0);
+        return result;
+    }
 }
